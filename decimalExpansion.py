765873c4a29b3700cc744ec4e653376a530a1b77
stimpfling <stimpfling@gmail.com>
2016-08-29 10:42:58 -0600

    Updated print statements to make output cleaner

--- decimalExpansion.py
+++ decimalExpansion.py
@@ -1,30 +1,34 @@
+import sys
+
 def decimalExpansion(num,denom):
     loopFound = False
     seenPairs = {}
     digits = []
+    position = 0
+
     wholeNum = num/denom
     remainder = num%denom
-    print str(wholeNum) + '.',
+
+    sys.stdout.write(str(wholeNum) + '.')
     if remainder == 0:
-        print "(0)"
+        sys.stdout.write('(0)')
         return
     
-    position = 0
     while(1):
         num = (remainder*10)/denom
         remainder = (remainder*10)%denom
         key = str(num) + str(remainder)
+
         if key in seenPairs.keys():
             startDigit = seenPairs[key]
             # First print non-looping digits (if any)
             for i in range(0,startDigit):
-                print str(digits[i]),
-
+                sys.stdout.write(str(digits[i]))
             # Now print looping digits in parenthesis
-            print "(",
+            sys.stdout.write('(')
             for i in range(startDigit,position):
-                print str(digits[i]),
-            print ")"
+                sys.stdout.write(str(digits[i]))
+            sys.stdout.write(')\n')
             break
         else:                
             seenPairs[key] = position
