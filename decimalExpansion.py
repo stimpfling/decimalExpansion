2b7b0dc3b05e87a4fc1f82f826455c40f9829633
stimpfling <stimpfling@gmail.com>
2016-08-29 10:50:35 -0600

    Final version

--- decimalExpansion.py
+++ decimalExpansion.py
@@ -1,20 +1,19 @@
 import sys
 
 def decimalExpansion(num,denom):
-    loopFound = False
+    if denom == 0:
+        print "Denominator cannot be 0"
+        return
+
     seenPairs = {}
     digits = []
     position = 0
 
     wholeNum = num/denom
     remainder = num%denom
-
     sys.stdout.write(str(wholeNum) + '.')
-    if remainder == 0:
-        sys.stdout.write('(0)')
-        return
     
-    while(1):
+    while(position < 5000):
         num = (remainder*10)/denom
         remainder = (remainder*10)%denom
         key = str(num) + str(remainder)
@@ -29,13 +28,17 @@ def decimalExpansion(num,denom):
             for i in range(startDigit,position):
                 sys.stdout.write(str(digits[i]))
             sys.stdout.write(')\n')
-            break
+            return
         else:                
             seenPairs[key] = position
             digits.append(num)
         position +=1
+    
+    print "No loop found after 5000 decimal places!"
 
 decimalExpansion(1,3)
 decimalExpansion(1,4)
 decimalExpansion(9,11)
 decimalExpansion(2,13)
+decimalExpansion(1,17)
+decimalExpansion(1,28)
